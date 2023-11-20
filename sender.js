fc3261722d111002223d7e862f3555b2ae4743e5
viKii <vikii@viKiis.local>
2023-11-20 19:09:43 +0530

    project added

---
+++ sender.js
@@ -0,0 +1,61 @@
+import express from 'express';
+import qrcode from 'qrcode-terminal';
+import pkg from 'whatsapp-web.js';
+
+import mysql from "mysql2/promise";
+const { Client, MessageMedia } = pkg;
+import cors from 'cors';
+import { resolve, join } from 'path';
+
+
+import csv from "csv-parser";
+import fs from "fs";
+
+const client = new Client({
+    puppeteer: {
+        args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    },
+});
+
+client.on("qr", (qr) => {
+    qrcode.generate(qr, { small: true });
+});
+
+client.on("ready", () => {
+    console.log("Client is ready!");
+    const phoneNumbers = [];
+    const messages = [];
+
+    // Read phone numbers and messages from CSV file
+    fs.createReadStream("messages.csv")
+        .pipe(csv())
+        .on("data", (data) => {
+            phoneNumbers.push("91" + data.phoneNumber + "@c.us");
+            messages.push(data.message);
+        })
+        .on("end", () => {
+            // Send messages to each phone number with delay of 2 seconds between each message
+            phoneNumbers.forEach((phoneNumber, index) => {
+                const message = messages[index];
+                // const message = "Hello,\nThis is a test message with a line break.";
+
+                setTimeout(() => {
+                    const resolvedAttachmentPath = resolve("/Users/vikii/Downloads/test.pdf");
+
+                    const media = MessageMedia.fromFilePath(resolvedAttachmentPath);
+
+                    client.sendMessage(phoneNumber, media, { caption: ' ' });
+                    console.log(`Processing number ${index + 1} of ${phoneNumbers.length}: ${phoneNumber}`);
+                    console.log(`Sent message to ${phoneNumber}`);
+                    console.log("-------------------------------");
+                }, (index + 1) * 2000); // delay in milliseconds
+            });
+        });
+});
+
+
+client.on("remote_session_saved", async () => {
+    console.log("Saved session");
+});
+
+client.initialize();
